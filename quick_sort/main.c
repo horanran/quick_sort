a3ac045d3f37556d69f277834c55d6811eedd35f
方舟 <fangzhou@fangzhoudeMacBook-Pro.local>
2015-05-03 13:30:34 +0800

    Initial Commit

---
+++ quick_sort/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  quick_sort
+//
+//  Created by 方舟 on 15/5/3.
+//  Copyright (c) 2015年 方舟. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
