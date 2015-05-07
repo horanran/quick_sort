627a73c1df5093a9db94547e685f007cb21c2bc2
Joe <fzark@163.com>
2015-05-07 10:31:54 +0800

    quick sort
    quick sort
    C

--- quick_sort/main.c
+++ quick_sort/main.c
@@ -7,9 +7,44 @@
 //
 
 #include <stdio.h>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int qsort_key(int *ar,int a,int b);
+void swap(int *a,int *b);
+void q_sort(int *ar,int a,int b);
+int main(void)
+{
+    int ar[] = {5,4,9,2,1};
+    q_sort(ar,0,4);
+    for(int i = 0; i < 5;i++)
+        printf("%d\t",ar[i]);
     return 0;
+}
+int qsort_key(int *ar,int a,int b)
+{
+    int i = a - 1;
+    int j;
+    for(j = a; j < b; j++)
+    {
+        if(ar[j] <= ar[b])
+        {
+            i++;
+            swap(&ar[i],&ar[j]);
+        }
+    }
+    swap(&ar[i+1],&ar[b]);
+    return i+1;
+}
+void swap(int *a,int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void q_sort(int *ar,int a,int b)
+{
+    if(b > a){
+        int c = qsort_key(ar, a, b);
+        q_sort(ar,a,c-1);
+        q_sort(ar,c+1,b);
+    }
 }
